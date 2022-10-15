7b12e159625a305191a51b2c550935a8c6d2aae6
Gyetvai Gabor <gyetvai.gabor@gmail.com>
2022-10-15 11:14:06 +0200

    game dir as a whole is not red

--- src/play/game/MakePuzzle.ts
+++ src/play/game/MakePuzzle.ts
@@ -1,6 +1,5 @@
 // https://stackoverflow.com/questions/8912917/cutting-an-image-into-pieces-through-javascript
 import { GameBuilder } from './builders/GameBuilder';
-import { GameHistoryService } from '../game-history.service';
 import { HintService } from '../hint.service';
 import { BackgroundBrightnessService } from '../background-brightness.service';
 
@@ -13,7 +12,6 @@ export class MakePuzzle {
         private sourceImageDataUrl: string,
         private cutX: number,
         private cutY: number,
-        private gameHistoryService: GameHistoryService,
         private imageId: number,
         hintService: HintService,
         brightnessService: BackgroundBrightnessService
@@ -22,13 +20,7 @@ export class MakePuzzle {
         this.sourceImage.src = this.sourceImageDataUrl;
         this.sourceImage.id = 'sourceImage';
         this.sourceImage.onload = (event: any) => {
-            this.gameBuilder = new GameBuilder(
-                this.sourceImage,
-                this.gameHistoryService,
-                this.imageId,
-                hintService,
-                brightnessService
-            );
+            this.gameBuilder = new GameBuilder(this.sourceImage, this.imageId, hintService, brightnessService);
             this.gameBuilder.initialize(this.cutX, this.cutY);
             this.gameBuilder.setViewHeight();
             this.gameBuilder.setResizedDimension();
